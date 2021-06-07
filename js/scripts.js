32578be62d12ca6213a43389ccf386658b2f2d07
Sarah dePalo <sarahdepalo99@gmail.com>
2021-06-07 14:54:26 -0400

    added basic random anime function

--- js/scripts.js
+++ js/scripts.js
@@ -21,33 +21,64 @@ document.addEventListener('DOMContentLoaded', function () {
         const random = Math.floor(Math.random() * restaurants.data.length);
         // Use restaurants.data[random].restaurant_name structure to grab data
         console.log(restaurants.data[random]);
-        
+        // need to add pieces here to update innerHTML
     }
-    const mainBtn = document.getElementById('mainBtn');
 
-    mainBtn.addEventListener('click', function() {
-        //Takes the input value and then sends it to the fetch request. 
-        let zipCodeInput = document.querySelector('input').value;
-        let zipCode = zipCodeInput
-        fetchRestaurants(zipCode)
-    })
 
-    function fetchAnime() {
-        fetch(`https://api.jikan.moe/v3/genre/anime/2`)
+    function grabGenre(genreChoice) {
+        // Matches the genre with the number from Jikan
+        let genre = 0;
+        if (genreChoice === "action") {
+            genre = 1;
+        }  else if (genreChoice === "comedy") {
+            genre = 4;
+        } else if (genreChoice === "drama") {
+            genre = 8;
+        } else if (genreChoice === "horror") {
+            genre = 14;
+        } else if (genreChoice === "romance") {
+            genre = 22;
+        }
+        else {
+            console.log('hello')
+        }
+        fetchAnime(genre);
+
+    }
+
+    function fetchAnime(genre) {
+        fetch(`https://api.jikan.moe/v3/genre/anime/${genre}`)
         .then(function(response) {
-            return response.json;
+            return response.json();
         })
-        .then(function(data) {
-            console.log(data.anime)
+        .then(function(animeList) {
+            randomizeAnime(animeList)
+            // console.log(animeList);
         })
         .catch(function (error) {
             console.error("ERROR: ", error);
         })
     }
 
-    fetchAnime();
+    function randomizeAnime(animeList) {
+        const randomAnime = Math.floor(Math.random() * animeList.anime.length);
+        console.log(animeList.anime[randomAnime]);
+        // Still need to add in the pieces to update innerHTML
+    }
 
-  
+    const mainBtn = document.getElementById('mainBtn');
+
+    mainBtn.addEventListener('click', function() {
+        //Takes the input value and then sends it to the fetch request. 
+        let zipCodeInput = document.querySelector('input').value;
+        let zipCode = zipCodeInput
+
+        // Takes the selected genre and sends it to grabGenre
+        const genreChoice = document.querySelector('select').value;
+
+        fetchRestaurants(zipCode)
+        grabGenre(genreChoice);
+    })
     
 
 })
